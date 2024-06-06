135d0e92ac32cfe03ad4ec15a29c514fbc9a402e
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2024-06-06 13:24:44 -0300

    Cleanup WriteSink

--- packages/vms-ingestion/vms_ingestion/normalization/transforms/write_sink.py
+++ packages/vms-ingestion/vms_ingestion/normalization/transforms/write_sink.py
@@ -10,8 +10,6 @@ class WriteSink(beam.PTransform):
                  labels,
                  ):
         self.table = table
-        self.schema = schema
-        self.description = description
         self.write_disposition = write_disposition
         self.labels = labels
 
@@ -25,12 +23,5 @@ class WriteSink(beam.PTransform):
         print(f'Writing Sink to {self.table}')
         return beam.io.WriteToBigQuery(
             table=self.table,
-            # schema=self.schema,
-            # additional_bq_parameters={
-            #     "destinationTableProperties": {
-            #         "description": self.description,
-            #     }
-            # },
             write_disposition=self.write_disposition,
-            # labels=self.labels,
         )
