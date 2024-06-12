fbea20eda3252764bd7fb79d8e4bfd8b2d247323
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2024-06-11 22:56:15 -0300

    Added Brazil normalization pipeline

--- packages/pipe-vms-ingestion/vms_ingestion/normalization/feed_pipeline_factory.py
+++ packages/pipe-vms-ingestion/vms_ingestion/normalization/feed_pipeline_factory.py
@@ -1,3 +1,4 @@
+from vms_ingestion.normalization.feeds.bra_pipeline import BRAFeedPipeline
 from vms_ingestion.normalization.feeds.chl_pipeline import CHLFeedPipeline
 from vms_ingestion.normalization.feeds.cri_pipeline import CRIFeedPipeline
 from vms_ingestion.normalization.pipeline import NormalizationPipeline
@@ -8,9 +9,11 @@ class FeedPipelineFactory:
     @staticmethod
     def get_pipeline(feed) -> NormalizationPipeline:
         feed_id = (feed or "").lower()
-        if feed_id == 'chl':
+        if feed_id == 'bra':
+            return BRAFeedPipeline
+        elif feed_id == 'chl':
             return CHLFeedPipeline
-        if feed_id == 'cri':
+        elif feed_id == 'cri':
             return CRIFeedPipeline
         else:
             raise ValueError(feed)
