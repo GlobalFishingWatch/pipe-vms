8f7ff09325509722717fa017120e717bfc553d94
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2024-08-09 17:47:50 -0300

    WIP ingest raw pan messages from excel to bq

---
+++ packages/pipe-vms-ingestion/vms_ingestion/ingestion/__init__.py
@@ -0,0 +1,25 @@
+from logger import logger
+from vms_ingestion.ingestion.excel_to_bq import excel_to_bq
+
+logger.setup_logger(1)
+logging = logger.get_logger()
+
+SUBCOMMANDS = {
+    "excel_to_bq": excel_to_bq,
+}
+
+
+def run_ingestion(argv):
+
+    if len(argv) < 2:
+        logging.info(
+            "No ingestion subcommand specified. Run pipeline ingestion [SUBCOMMAND], "
+            + "where subcommand is one of %s",
+            SUBCOMMANDS.keys(),
+        )
+        exit(1)
+
+    subcommand = argv[0]
+    subcommand_args = argv[1:]
+
+    SUBCOMMANDS[subcommand](subcommand_args)
---
+++ packages/pipe-vms-ingestion/vms_ingestion/ingestion/excel_to_bq/__init__.py
@@ -0,0 +1,38 @@
+from common.pipeline import (
+    build_pipeline_options_with_defaults,
+    is_blocking_run,
+    success_states,
+)
+from logger import logger
+from vms_ingestion.ingestion.excel_to_bq.options import IngestionExcelToBQOptions
+from vms_ingestion.ingestion.excel_to_bq.pipeline import IngestionExcelToBQPipeline
+
+logger.setup_logger(1)
+logging = logger.get_logger()
+
+
+def excel_to_bq(argv):
+
+    logging.info("Running excel to bq ingestion dataflow pipeline with args %s", argv)
+
+    logging.info("Building pipeline options")
+    options = build_pipeline_options_with_defaults(argv).view_as(
+        IngestionExcelToBQOptions
+    )
+
+    logging.info("Launching pipeline")
+    pipeline = IngestionExcelToBQPipeline(options=options)
+    result = pipeline.run()
+
+    if is_blocking_run(options, IngestionExcelToBQOptions):
+        logging.info("Waiting for job to finish")
+        result.wait_until_finish()
+
+    logging.info("Pipeline launch result %s", result.state)
+
+    if result.state in success_states(options, IngestionExcelToBQOptions):
+        logging.info("Terminating process successfully")
+        exit(0)
+    else:
+        logging.info("Terminating process with error")
+        exit(1)
