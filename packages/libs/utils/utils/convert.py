e8fc40490c84d7ea1e729921a30e8ec95ff8a94b
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2024-08-16 14:04:13 -0300

    Moved dms_to_float conversion function to utils lib

--- packages/libs/utils/utils/convert.py
+++ packages/libs/utils/utils/convert.py
@@ -1,2 +1,22 @@
 def to_float(value):
     return float(value) if value is not None else None
+
+
+def dms_to_float(dms_value: str):
+    """
+    Convert Degrees, Minutes, Seconds coordinate to float value
+
+    :param dms_value: str Coordinate value sexpressed in Degrees, Minutes, Seconds
+    :return: float Coordinate value converted to float
+    """
+    dms_value = dms_value.strip()
+    degrees, rest = dms_value.split("°")
+    minutes, rest = rest.split("'")
+    seconds, direction = rest.split('" ')
+    degrees = float(degrees)
+    minutes = float(minutes)
+    seconds = float(seconds)
+    float_value = degrees + minutes / 60 + seconds / 3600
+    if direction in ["S", "W"]:
+        float_value = -float_value
+    return float_value
