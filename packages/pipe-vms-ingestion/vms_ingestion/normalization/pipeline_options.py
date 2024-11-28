4ccd46fd4e5c63839b0eefbb29ca5643e29ab23b
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2024-11-28 15:17:09 -0300

    Removed affected_entities argument

--- packages/pipe-vms-ingestion/vms_ingestion/normalization/pipeline_options.py
+++ packages/pipe-vms-ingestion/vms_ingestion/normalization/pipeline_options.py
@@ -9,25 +9,6 @@ class Entities(str, Enum):
     VESSEL_INFO = "vessel_info"
 
 
-def validate_affected_entities(value):
-    accepted_values = [Entities.POSITIONS, Entities.VESSEL_INFO]
-
-    entities = value.split(",") if isinstance(value, str) else []
-    if len(entities) == 0:
-        raise argparse.ArgumentTypeError(
-            "Invalid affected entities value. At least one entity must be provided"
-        )
-
-    for entity in entities:
-        if entity not in accepted_values:
-            quoted_accepted_values = [f'"{v}"' for v in accepted_values]
-            raise argparse.ArgumentTypeError(
-                f'Invalid entity name "{entity}". Valid options are {", ".join(quoted_accepted_values)}'
-            )
-
-    return value
-
-
 class NormalizationOptions(CommonPipelineOptions):
     @classmethod
     def _add_argparse_args(cls, parser: argparse.ArgumentParser):
@@ -39,12 +20,4 @@ class NormalizationOptions(CommonPipelineOptions):
             "Defaults to the same project and dataset provided in [destination] with reported_vessel_info "
             "table name.",
             default="reported_vessel_info",
-        )
-
-        optional.add_argument(
-            "--affected_entities",
-            required=False,
-            type=validate_affected_entities,
-            default=",".join([Entities.POSITIONS, Entities.VESSEL_INFO]),
-            help="Which entities will be stored by this pipeline",
         )
