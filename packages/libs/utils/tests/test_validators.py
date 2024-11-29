dd01a3a0079e8c9d0091af965570604d87d44b2f
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2024-11-29 10:22:11 -0300

    Added tests for is_valid_mmsi

--- packages/libs/utils/tests/test_validators.py
+++ packages/libs/utils/tests/test_validators.py
@@ -1,10 +1,8 @@
 import argparse
 import unittest
+from datetime import datetime
 
-from utils.validators import (
-    check_if_a_key_exists_in_a_dic,
-    check_if_it_is_a_valid_YYYYMMDD,
-)
+from utils.validators import check_if_a_key_exists_in_a_dic, check_if_it_is_a_valid_YYYYMMDD, is_valid_mmsi
 
 
 class TestValidators(unittest.TestCase):
@@ -27,6 +25,23 @@ class TestValidators(unittest.TestCase):
     def test_check_if_it_is_a_valid_YYYYMMDD_with_invalid_input(self):
         with self.assertRaises(argparse.ArgumentTypeError):
             check_if_it_is_a_valid_YYYYMMDD("wrong_date")
+
+    def test_is_valid_mmsi(self):
+        self.assertFalse(is_valid_mmsi(datetime.now()), "mmsi should be a string or a number, not a datetime")
+        self.assertFalse(is_valid_mmsi(True), "mmsi should be a string or a number, not a boolean")
+        self.assertFalse(is_valid_mmsi(""), "mmsi should be a number between 200000000 and 799999999")
+        self.assertFalse(is_valid_mmsi("abcdefghi"), "mmsi should be a number between 200000000 and 799999999")
+
+        self.assertFalse(is_valid_mmsi("199999999"), "mmsi should be a number between 200000000 and 799999999")
+        self.assertFalse(is_valid_mmsi(199999999), "mmsi should be a number between 200000000 and 799999999")
+        self.assertFalse(is_valid_mmsi("800000000"), "mmsi should be a number between 200000000 and 799999999")
+        self.assertFalse(is_valid_mmsi(800000000), "mmsi should be a number between 200000000 and 799999999")
+
+        self.assertTrue(is_valid_mmsi("200000000"), "mmsi should be a number between 200000000 and 799999999")
+        self.assertTrue(is_valid_mmsi(200000000), "mmsi should be a number between 200000000 and 799999999")
+        self.assertTrue(is_valid_mmsi("799999999"), "mmsi should be a number between 200000000 and 799999999")
+        self.assertTrue(is_valid_mmsi(799999999), "mmsi should be a number between 200000000 and 799999999")
+        self.assertTrue(is_valid_mmsi("200000000"), "mmsi should be a number between 200000000 and 799999999")
 
 
 if __name__ == "__main__":
