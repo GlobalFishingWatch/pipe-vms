bcc5a8f167e54818f8c0494346f2e2226ff1077d
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2024-11-29 10:55:27 -0300

    Added tests for mmsi_to_iso3166_alpha3

--- packages/libs/utils/tests/test_convert.py
+++ packages/libs/utils/tests/test_convert.py
@@ -1,6 +1,6 @@
 import unittest
 
-from utils.convert import to_float
+from utils.convert import mmsi_to_iso3166_alpha3, to_float
 
 
 class TestConvert(unittest.TestCase):
@@ -15,6 +15,20 @@ class TestConvert(unittest.TestCase):
     def test_with_invalid_input(self):
         with self.assertRaises(ValueError):
             to_float("wrong_float")
+
+    def test_mmsi_to_iso3166_alpha3_valid(self):
+        # Assuming mmsi_mid_codes contains {'369': [None, 'USA', None, None]}
+        result = mmsi_to_iso3166_alpha3('369456789')
+        self.assertEqual(result, 'USA')
+
+    def test_mmsi_to_iso3166_alpha3_invalid_mmsi(self):
+        result = mmsi_to_iso3166_alpha3('invalid_mmsi')
+        self.assertIsNone(result)
+
+    def test_mmsi_to_iso3166_alpha3_not_in_mid_codes(self):
+        # Assuming mmsi_mid_codes does not contain '999'
+        result = mmsi_to_iso3166_alpha3('999456789')
+        self.assertIsNone(result)
 
 
 if __name__ == "__main__":
